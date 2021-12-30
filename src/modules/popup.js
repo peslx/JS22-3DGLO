7acf65da7c7203cd530ff01c34b098dd3e7c5644
peslx <90468331+peslx@users.noreply.github.com>
2021-12-30 18:57:12 +0900

    Реализовано открытие-закрытие модального окна

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,5 +1,18 @@
 const popup = () => {
   console.log("'popup.js' подключен");
+  const popup = document.querySelector(".popup");
+  const triggers = document.querySelectorAll(".popup-btn");
+  const closeBtn = popup.querySelector(".popup-close");
+
+  triggers.forEach((trigger) => {
+    trigger.addEventListener("click", () => {
+      popup.style.display = "block";
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
 };
 
 export default popup;
