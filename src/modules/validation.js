fe3e5c1372a8fbb5c6499801e4e7eb3a66564114
peslx <90468331+peslx@users.noreply.github.com>
2022-01-02 21:07:29 +0300

    Реализована валидация формы с калькулятором

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,5 +1,31 @@
 const validation = () => {
   console.log("'validation.js' подключен");
+
+  const calcInputs = document
+    .querySelector(".calc-block")
+    .querySelectorAll("input");
+
+  const mainFormInputs = document
+    .querySelector(".main-form-input")
+    .querySelectorAll("input");
+
+  const footerFormInputs = document
+    .querySelector(".footer-form-input")
+    .querySelectorAll("input");
+
+  const popupFormInputs = document
+    .getElementById("form3")
+    .querySelectorAll("input");
+
+  console.log(mainFormInputs);
+  console.log(footerFormInputs);
+  console.log(popupFormInputs);
+
+  calcInputs.forEach((i) => {
+    i.addEventListener("input", () => {
+      i.value = i.value.replace(/\D/g, "");
+    });
+  });
 };
 
 export default validation;
