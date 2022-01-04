331506633dd8b4a27a2ed07d2c676d02440a96e7
peslx <90468331+peslx@users.noreply.github.com>
2022-01-04 19:06:07 +0900

    Получены основные элементы для работы калькулятора

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,5 +1,19 @@
 const calc = () => {
   console.log("'calc.js' подключен");
+  const calc = document.querySelector(".calc-block");
+  const calcType = document.querySelector(".calc-type");
+  const calcSquare = document.querySelector(".calc-square");
+  const calcCount = document.querySelector(".calc-count");
+  const calcDay = document.querySelector(".calc-day");
+  const calcTotal = document.getElementById("total");
+
+  const runCalc = () => {
+    console.log("total value");
+  };
+
+  calc.addEventListener("input", (e) => {
+    runCalc();
+  });
 };
 
 export default calc;
