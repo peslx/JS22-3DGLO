71353894f47b00d85df66ba94c9c107b9552e3ca
peslx <90468331+peslx@users.noreply.github.com>
2022-01-04 19:56:27 +0900

    Реализован калькулятор

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,4 +1,4 @@
-const calc = () => {
+const calc = (price = 100) => {
   console.log("'calc.js' подключен");
   const calc = document.querySelector(".calc-block");
   const calcType = document.querySelector(".calc-type");
@@ -8,7 +8,37 @@ const calc = () => {
   const calcTotal = document.getElementById("total");
 
   const runCalc = () => {
-    console.log("total value");
+    const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+    const calcSquareValue = +calcSquare.value;
+    let calcCountValue = 1;
+    let calcDayValue;
+
+    if (+calcCount.value > 1) {
+      calcCountValue += +calcCount.value / 10;
+    }
+
+    if (+calcDay.value === 0) {
+      calcDayValue = 1;
+    } else if (+calcDay.value < 5) {
+      calcDayValue = 2;
+    } else if (+calcDay.value < 10) {
+      calcDayValue = 1.5;
+    } else {
+      calcDayValue = 1;
+    }
+
+    if (calcTypeValue && calcSquareValue) {
+      const calcTotalValue =
+        price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
+
+      console.log(
+        `${price} * ${calcTypeValue} * ${calcSquareValue} * ${calcCountValue} * ${calcDayValue} = ${calcTotalValue}`
+      );
+
+      calcTotal.textContent = calcTotalValue;
+    }
+
+    //
   };
 
   calc.addEventListener("input", (e) => {
